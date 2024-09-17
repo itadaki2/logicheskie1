4633f30da34c21a6622fd55d7f627a22d12393a8
shdwvrs <yakyzaanengs22@gmail.com>
2024-09-17 13:05:12 +0900

    log1

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val a = true
+    val b = false
+    val c = false
+
+    println("a || b: ${a || b}") // a или b
+    println("a && b: ${a && b}") // a и b
+    println("b || c: ${b || c}") // b или c
+}
